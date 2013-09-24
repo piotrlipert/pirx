fa0fe53643bdf67bd09e0aaf50d5a2a7133d5a5a
Maciej Jasiński <maciejjasinski@MacBook-Pro-Maciej.local>
2013-09-24 20:10:32 +0200

    added all 7 parallax separator images

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,20 +1,24 @@
 $(document).ready(function() {
 
-	$('.faq1, .faq2, .faq3, .faq4').hide();
+	$('.faq2, .faq3, .faq4').hide();
 	$('.faq-1').click(function(){
 		$('.faq1, .faq2, .faq3, .faq4').hide();
-		$('.faq1').show().addClass('active');
+		$('.faq1').show();
+		$('.faq-1').addClass('active');
 	});
 	$('.faq-2').click(function(){
 		$('.faq1, .faq2, .faq3, .faq4').hide();
 		$('.faq2').show().addClass('active');
+		$('.faq-2').addClass('active');
 	});
 	$('.faq-3').click(function(){
 		$('.faq1, .faq2, .faq3, .faq4').hide();
 		$('.faq3').show().addClass('active');
+		$('.faq-3').addClass('active');
 	});
 	$('.faq-4').click(function(){
 		$('.faq1, .faq2, .faq3, .faq4').hide();
 		$('.faq4').show().addClass('active');
+		$('.faq-4').addClass('active');
 	});
 });
