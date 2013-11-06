b35177371ddf9c5400fa60a3160c1f9ac95944dd
Piotr Lipert <piotr.lipert@gmail.com>
2013-11-06 17:24:51 +0100

    TERMIT

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
 
 $headers = apache_request_headers();
-
+$a = 0;
 foreach ($headers as $header => $value) {
     echo "$header: $value <br />\n";
+    if($header=='Host')
+    {
+    	if($value=='pirx3d.pl')
+    		$a = 1;
+    	if($value=='www.pirx3d.pl')
+    		$a = 1;
+    }
 }
-#include_once('index_en.html');
-
+if($a==0)
+	include_once('index_en.html');
+else
+	include_once('index_pl.html');
 
 ?>
