281d4e13b1a66c962aed416197dc003679d1e43f
Piotr Lipert <piotr.lipert@gmail.com>
2013-11-07 19:03:18 +0100

    saddsad

--- scripts/sendmail.php
+++ scripts/sendmail.php
@@ -3,17 +3,21 @@
 echo "HEJ!";
 
 require_once 'swift/lib/swift_required.php';
+echo "HEJ!";
 
 $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
   ->setUsername('support@pirx3d.com')
   ->setPassword('uchozoid1a');
+echo "HEJ!";
 
 $mailer = Swift_Mailer::newInstance($transport);
+echo "HEJ!";
 
 $message = Swift_Message::newInstance('Test Subject')
   ->setFrom(array('abc@example.com' => 'ABC'))
   ->setTo(array('piotr.lipert@gmail.com'))
   ->setBody('This is a test mail.');
+echo "HEJ!";
 
 $result = $mailer->send($message);
 
