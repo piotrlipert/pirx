e75e79f3c892fc300f7600f7d758a615cbfd5ff3
Piotr Lipert <piotr.lipert@gmail.com>
2013-11-07 19:20:04 +0100

    asdasdasd

--- scripts/sendmail.php
+++ scripts/sendmail.php
@@ -1,12 +1,21 @@
 <?php
 
+	
+	
+	
+
 require("postmark.php");
+echo "BRATPIT"	
+	$postmark = new Postmark("64229856-c00b-4086-b886-24aefc8a9ae7","support@pirx3d.com","optional-reply-to-address");
 	
-	$postmark = new Postmark("64229856-c00b-4086-b886-24aefc8a9ae7","pirx@pirx3d.com","optional-reply-to-address");
+	$result = $postmark->to("reciver@example.com")
+		->subject("Email Subject")
+		->plain_message("This is a plain text message.")
+		#->attachment('File.pdf', $file_as_string, 'application/pdf')
+		->send();
 	
-	if($postmark->to("piotr.lipert@gmail.com")->subject("Email Subject")->plain_message("This is a plain text message.")->send()){
+	if($result === true)
 		echo "Message sent";
-	}
 
 
 ?>
