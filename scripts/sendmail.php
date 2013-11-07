b5ab2cc35729952c2522f0593ea9a4813964ab51
Piotr Lipert <piotr.lipert@gmail.com>
2013-11-07 18:51:42 +0100

    TEST
    asd

--- scripts/sendmail.php
+++ scripts/sendmail.php
@@ -2,4 +2,34 @@
 
 echo "HEJ!";
 
+// Pear Mail Library
+require_once "Mail/Mail.php";
+
+$from = '<support@pirx3d.com>';
+$to = '<piotr.lipert@gmail.com>';
+$subject = 'Hi!';
+$body = "Hi,\n\nHow are you?";
+
+$headers = array(
+    'From' => $from,
+    'To' => $to,
+    'Subject' => $subject
+);
+
+$smtp = Mail::factory('smtp', array(
+        'host' => 'ssl://smtp.gmail.com',
+        'port' => '465',
+        'auth' => true,
+        'username' => 'support@pirx3d.com',
+        'password' => 'uchozoid1a'
+    ));
+
+$mail = $smtp->send($to, $headers, $body);
+
+if (PEAR::isError($mail)) {
+    echo('<p>' . $mail->getMessage() . '</p>');
+} else {
+    echo('<p>Message successfully sent!</p>');
+}
+
 ?>
