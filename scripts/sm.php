fccbb567d73d48803148166e8dd6a1c5327c150b
Piotr Lipert <piotr.lipert@gmail.com>
2013-11-07 20:08:51 +0100

    sdasddas

--- scripts/sm.php
+++ scripts/sm.php
@@ -3,7 +3,7 @@
 require("postmark.php")
 
 	
-	$postmark = new Postmark("64229856-c00b-4086-b886-24aefc8a9ae7","support@pirx3d.com","optional-reply-to-address");
+	$postmark = new Postmark("64229856-c00b-4086-b886-24aefc8a9ae7","support@pirx3d.com","support@pirx3d.com");
 	
 	$result = $postmark->to("piotr.lipert@gmail.com")
 		->subject("Email Subject")
