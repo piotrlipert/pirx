4ff022cbe7863107d73b09203fef00c11d3eda8c
Piotr Lipert <piotr.lipert@gmail.com>
2013-11-07 19:53:36 +0100

    asas
    asas

--- scripts/sendmail.php
+++ scripts/sendmail.php
@@ -1,6 +1,9 @@
 
 
 <?php
+
+require("postmark.php")
+
 echo "SASASD";
 
 ?>
--- scripts/sm.php
+++ scripts/sm.php
@@ -5,10 +5,9 @@ require("postmark.php")
 	
 	$postmark = new Postmark("64229856-c00b-4086-b886-24aefc8a9ae7","support@pirx3d.com","optional-reply-to-address");
 	
-	$result = $postmark->to("reciver@example.com")
+	$result = $postmark->to("piotr.lipert@gmail.com")
 		->subject("Email Subject")
 		->plain_message("This is a plain text message.")
-		->attachment('File.pdf', $file_as_string, 'application/pdf')
 		->send();
 	
 	if($result === true)
