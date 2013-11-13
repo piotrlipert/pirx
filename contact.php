550e755f909124d3de4f8caba196ac7d13074a51
Piotr Lipert <piotr.lipert@gmail.com>
2013-11-13 14:17:09 +0100

    sasaas

--- contact.php
+++ contact.php
@@ -73,7 +73,39 @@
             </ul>
           </div>
           <div class="large-9 columns contact-us">
+
+<script>
+    $(function(){
+      $('form').submit(function(e){
+        var thisForm = $(this);
+        //Prevent the default form action
+        e.preventDefault();
+        //Hide the form
+        $(this).fadeOut(function(){
+          //Display the "loading" message
+          $("#loading").fadeIn(function(){
+            //Post the form to the send script
+            $.ajax({
+              type: 'POST',
+              url: thisForm.attr("action"),
+              data: thisForm.serialize(),
+              //Wait for a successful response
+              success: function(data){
+                //Hide the "loading" message
+                $("#loading").fadeOut(function(){
+                  //Display the "success" message
+                  $("#success").text(data).fadeIn();
+                });
+              }
+            });
+          });
+        });
+      })
+    });
+  </script>
+
 	          <h2>Drop us a message</h2>
+
 
            
             <form action="scripts/sm.php" method="POST">
