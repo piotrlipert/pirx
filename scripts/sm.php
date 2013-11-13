05f9ecadca6dbe58dd3db134084f7bd34365d24c
Piotr Lipert <piotr.lipert@gmail.com>
2013-11-13 14:14:26 +0100

    asas

--- scripts/sm.php
+++ scripts/sm.php
@@ -12,7 +12,7 @@ require("postmark.php");
 
 
 
-	$postmark = new Postmark("64229856-c00b-4086-b886-24aefc8a9ae7",$email,$email);
+	$postmark = new Postmark("64229856-c00b-4086-b886-24aefc8a9ae7","support@pirx3d.com",$email);
 	
 	$result = $postmark->to("pirx@pirx3d.com")
 		->subject("Email from Pirx3D")
