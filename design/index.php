a9deb7b32d5c75bd331b2be54c14b481fbef885b
Piotr Lipert <piotr.lipert@gmail.com>
2013-12-17 15:22:04 +0100

    asd
    dsa

---
+++ design/index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Pirx 3d Printer - Press Kit</title>
+
+    <!-- META DATA -->
+    <meta charset="utf-8">
+    
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta name="description" content="Pirx 3d Printer">
+    <meta name="keywords" content="pirx, 3d, printer, 3d printer, 3d print, community, pirx3d, hardware">  
+
+    <!-- FONTS -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+
+    <!-- STYLESHEETS -->
+    <link href="../styles/normalize.css" rel="stylesheet">
+    <link href="../styles/foundation.css" rel="stylesheet">
+    <link href="../styles/style.css" rel="stylesheet">
+    <link href="../styles/lightbox.css" rel="stylesheet">
+    
+    <!-- JAVASCRIPT -->
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script type="text/javascript" src="../js/jquery.stellar.js"></script>
+    <script type="text/javascript" src="../js/retina.js"></script>
+    <script type="text/javascript" src="../js/lightbox-2.6.min.js"></script>
+    <script type="text/javascript" src="../js/functions.js"></script>
+
+    <!-- FAVICON -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+
+    <!-- JS FUNCTIONS -->   
+
+  </head>
+  <body>
+    <div id="wrapper">
+      <header id="header">
+        <div class="row">
+          <div class="large-3 small-12 columns">
+            <div class="logo">
+            </div>
+          </div>
+          <div class="large-9 small-12 columns">
+            <ul class="nav right">
+              <li><a href="/">home</a></li>
+              <li><a href="../about/">about</a></li>
+              <li><a href="https://facebook.com/clockwork3d">blog</a></li>
+              <li><a href="../contact/">contact</a></li>
+              <li><a href="" class="btn btn-small btn-order">buy now</a></li>
+            </ul>
+          </div>
+        </div>
+      </header>
+
+      <section id="caption">
+        <div class="row caption">
+          <div class="large12 columns">
+            <h2>Press Kit</h2>
+          </div>
+        </div>
+      </section>
+
+      <section id="about-us">
+        <div class="row">
+          <div class="large-3 columns">
+            <ul class="nav sidebar-nav">
+              <li><a href="../">Home<span></span></a></li>
+              <li><a href="../about/">About<span></span></a></li>
+              <li><a href="#" class="active">Press Kit<span></span></a></li>
+              <li><a href="../contact/">Contact<span></span></a></li>
+            </ul>
+          </div>
+          <div class="large-9 columns press">
+            <h2>Pirx 3d Printer</h2>
+            <p>If you'd like to write something about us, or use our branding in any different way, please use this resources.</p>
+
+            
+
+            <h2>Press Materials</h2>
+           
+            <div class="row">
+              <div class="large-6 columns">
+                <img src="../images/press-3-th.png" alt="">
+                <a href="../assets/printer1.jpg">Download Pirx 3d Printer (JPG)</a>
+              </div>
+              <div class="large-6 columns">
+                <img src="../images/press-4-th.png" alt="">
+                <a href="../assets/printer2.jpg">Download Pirx 3d Printer Interior (JPG)</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <footer id="footer">
+        <div class="row">
+          <div class="large-3 columns">
+            <h3>Pirx</h3>
+            <ul>
+              <li><a href="../about/">Meet the team</a></li>
+              <li><a href="../contact/">Support</a></li>
+            </ul>
+          </div>
+          <div class="large-3 columns">
+            <h3>Media</h3>
+            <ul>
+              <li><a href="https://www.facebook.com/clockwork3d">Blog</a></li>
+              <li><a href="../press/">Press kit</a></li>
+              <!-- <li>Privacy policy</li>
+              <li>Terms & Conditions</li> -->
+            </ul>
+          </div>
+          <div class="large-3 columns">
+            <h3>Newsletter</h3>
+            <p>Subscribe to the newsletter to be up to date with our stuff.</p>
+
+            <form action="http://pirx3d.us3.list-manage.com/subscribe/post?u=29dd98989c038680bd9fdbcec&amp;id=46fdd7bcd3" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+            <button class="btn btn-small btn-order"input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" >send
+            </button>
+          </div>
+          <div class="large-3 columns address">
+            <h3>Contact</h3>
+            <address>
+              Pirx<br>  
+              Berka Joselewicza 21<br>
+              31-031 Kraków<br>
+              Małopolska, Poland
+            </address>
+          </div>
+        </div>
+        <div class="row">
+          <div class="large-6 large-centered columns social">
+            <ul class="social-icons">
+              <li><a class="fb"></a></li>
+              <li><a class="tw"></a></li>
+              <li><a class="gp"></a></li>
+              <li><a class="yt"></a></li>
+              <li><a class="vi"></a></li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+
+      <footer id="copyright">
+        <div class="row">
+          <div class="large-6 columns"><p>© 2013 Pirx</p></div>
+          <div class="large-6 columns"><p class="right">Handcrafted in Kraków</p></div>
+        </div>
+      </footer>
+    </div>
+  </body>
+</html>
