ea7eb779cbfdb2cafc1b7c0690f989f5aed954df
Piotr Lipert <piotr.lipert@gmail.com>
2014-01-20 12:28:29 +0100

    Link masking
    And path updates.

--- recruit/office/index.php
+++ recruit/office/index.php
@@ -15,24 +15,24 @@
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
 
     <!-- STYLESHEETS -->
-		<link href="../styles/normalize.css" rel="stylesheet">
-    <link href="../styles/foundation.css" rel="stylesheet">
-    <link href="../styles/style.css" rel="stylesheet">
-    <link href="../styles/lightbox.css" rel="stylesheet">
+		<link href="../../styles/normalize.css" rel="stylesheet">
+    <link href="../../styles/foundation.css" rel="stylesheet">
+    <link href="../../styles/style.css" rel="stylesheet">
+    <link href="../../styles/lightbox.css" rel="stylesheet">
     
     <!-- JAVASCRIPT -->
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-    <script type="text/javascript" src="../js/jquery.stellar.js"></script>
-    <script type="text/javascript" src="../js/retina.js"></script>
-    <script type="text/javascript" src="../js/lightbox-2.6.min.js"></script>
-    <script type="text/javascript" src="../js/functions.js"></script>
+    <script type="text/javascript" src="../../js/jquery.stellar.js"></script>
+    <script type="text/javascript" src="../../js/retina.js"></script>
+    <script type="text/javascript" src="../../js/lightbox-2.6.min.js"></script>
+    <script type="text/javascript" src="../../js/functions.js"></script>
     
 		<!-- FAVICON -->
-    <link rel="shortcut icon" href="../images/favicon.ico">
+    <link rel="shortcut icon" href="../../images/favicon.ico">
 
     <!-- JS FUNCTIONS -->   
     
-    <script type="text/javascript" src="../scripts/recruitscript.js"></script>
+    <script type="text/javascript" src="../../scripts/recruitscript.js"></script>
 
   </head>
   <body>
@@ -45,11 +45,11 @@
           </div>
           <div class="large-9 small-12 columns">
             <ul class="nav right">
-              <li><a href="../">home</a></li>
-              <li><a href="../about/">about</a></li>
+              <li><a href="../../">home</a></li>
+              <li><a href="../../about/">about</a></li>
               <li><a href="https://facebook.com/clockwork3d">blog</a></li>
-              <li><a href="../contact/">contact</a></li>
-              <li><a href="../order/" class="btn btn-small btn-order">buy now</a></li>
+              <li><a href="../../contact/">contact</a></li>
+              <li><a href="../../order/" class="btn btn-small btn-order">buy now</a></li>
             </ul>
           </div>
         </div>
@@ -58,7 +58,7 @@
       <section id="caption">
         <div class="row caption">
           <div class="large12 columns">
-            <h2>Expension Officer</h2>
+            <h2>Office Manager</h2>
           </div>
         </div>
       </section>
@@ -67,13 +67,13 @@
         <div class="row">
           <div class="large-3 columns">
             <ul class="nav sidebar-nav">
-              <li><a href="../">Home<span></span></a></li>
-              <li><a href="../about/">About<span></span></a></li>
-              <li><a href="../order/">Order<span></span></a></li>
-              <li><a href="../recruit/">Join us!<span></span></a>
+              <li><a href="../../">Home<span></span></a></li>
+              <li><a href="../../about/">About<span></span></a></li>
+              <li><a href="../../order/">Order<span></span></a></li>
+              <li><a href="../../recruit/">Join us!<span></span></a>
                 <ul class="sidebar-subnav">
-                  <li><a href="#" class="active">Expension Officer<span></span></a></li>
-                  <li><a href="office-manager.html">Office Manger<span></span></a></li>
+                  <li><a href="expension-officer.html">Expension Officer<span></span></a></li>
+                  <li><a href="#" class="active">Office Manger<span></span></a></li>
                 </ul>
               </li>
               <li><a href="../press/">Press Kit<span></span></a></li>
@@ -81,7 +81,7 @@
             </ul>
           </div>
           <div class="large-9 columns jobs">
-	          <h2>We are looking for an Expension Officer.</h2>
+	          <h2>We are looking for an Office Manager.</h2>
 
             <p>If you are a highly creative 
             person with amazing communication and copywriting skills please fill out
@@ -96,46 +96,42 @@
 
             <form action="../scripts/recruit.php" method="POST" name="recruitform" id="recruitform">
               <fieldset>    
-                <div class="question">
+                <label>
 	                <p>Are you ready to beat unfavorable odds, carry out seemingly impossible tasks and work with us to change the world?</p>
-                  <button type="checkbox" name="sane" id="sane" class="btn btn-small btn-order" onClick="is_sane()">Yes I am!</button>
-                </div>
+	                <input type = "checkbox" name="sane" id="sane" onClick="is_sane()">
+                </label>
 
 	              <div id="form_section" style="display: none;">
-	                <label><span>1</span>What is your name?</label>
+	                <label>What is your name?</label>
 	                <input type = "text" name="name" id="name">
 
-	                <label><span>2</span>Have you ever dreamt of space travel?</label>
-                  <div class="check">
-  	                <input type="checkbox" name="space" id="space" onClick="" class="lite-orange-check">
-                  </div>
+	                <label>Have you ever dreamt of space travel?</label>
+	                <input type = "checkbox" name="space" id="space" onClick="">
 
-	                <label><span>3</span>Do you want to be filthy rich?</label>
-	                <div class="check">
-                    <input type="checkbox" name="rich" id="rich" onClick="" class="lite-orange-check">
-                  </div>
+	                <label>Do you want to be filthy rich?</label>
+	                <input type = "checkbox" name="rich" id="rich" onClick="">
 
-	                <label><span>4</span>You have a week and no budget at all. Your goal is to reach as many potential Pirx customers as you can. Be creative. Bonus points for scenarios that end up with sold printers.</label>   
-	                <textarea type="text" name="question_1" id="question1"></textarea>
+	                <label>You have a week and no budget at all. Your goal is to reach as many potential Pirx customers as you can. Be creative. Bonus points for scenarios that end up with sold printers.
+	                </label>
 	                
-                  <label><span>5</span>You are a prisoner of a nasty cannibal tribe. Things went very bad
+	                <textarea type="text" name="question_1" id="question1"></textarea>
+	                <label>You are a prisoner of a nasty cannibal tribe. Things went very bad
 	                 and they are just about to cook you in a large, brass cauldron. You are hanging upside down by the ankle, all tied up. Somehow you miraculously managed to free one hand. Just by accident, there is a 
 	                 laptop connected to a Pirx 3D printer just within your reach. What will you do to save yourself?
 	                </label>
+	                
 	                <textarea type="text" name="question_2" id="question2"></textarea>
+	                <label>What amazing thing would you print on Pirx?</label>
 	                
-                  <label><span>6</span>What amazing thing would you print on Pirx?</label>
 	                <textarea type="text" name="question_3" id="question3"></textarea>
+	                <label>How would you market our 3D printer to Bruce Wayne?</label>
 	                
-                  <label><span>7</span>How would you market our 3D printer to Bruce Wayne?</label>
 	                <textarea type="text" name="question_4" id="question4"></textarea>
-
-	                <label><span>8</span>Email address:</label>
+	                <label>Email address:</label>
 	                <input type = "text" name="email" id="email">
 	                  
-	                <label><span>9</span>Is there anything else we need to know?</label>
+	                <label>Is there anything else we need to know?</label>
 	                <textarea type="text" name='message' placeholder=""></textarea>
-
 	                <button type="submit" class="btn btn-small btn-order">Apply for this position</button>
                 </div>
               </fieldset>
@@ -162,10 +158,10 @@
           <div class="large-3 columns">
             <h3>Pirx</h3>
             <ul>
-              <li><a href="../about/">Meet the team</a></li>
-              <li><a href="../order/">Buy your printer</a></li>
-              <li><a href="../recruit">Join the crew</a></li>
-              <li><a href="../contact/">Ask a question</a></li>
+              <li><a href="../../about/">Meet the team</a></li>
+              <li><a href="../../order/">Buy your printer</a></li>
+              <li><a href="../../recruit">Join the crew</a></li>
+              <li><a href="../../contact/">Ask a question</a></li>
             </ul>
           </div>
           <div class="large-3 columns">
